82164c054eb20276fcc1b2eb425f76c61eadcb3b
Akram Louze <akramlouze2005@gmail.com>
2025-06-29 16:32:39 +0100

    Update proxy to support POST requests

--- api/proxy.js
+++ api/proxy.js
@@ -1,29 +1,46 @@
 /**
  * Handle incoming requests to accept CORS
  */
+export const config = {
+    api: {
+        bodyParser: false
+    },
+};
+
 export default async function handler(req, res) {
-    const { url, method, headers, body } = req;
+    const { method, headers } = req;
+
+    // Handle CORS preflight in case of OPTIONS request
+    if (method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.status(204).end();
+        return;
+    }
 
     const path = req.url.replace(/^\/+/, '');
     const baseUrl = 'https://api-webetu.mesrs.dz/';
     const finalUrl = baseUrl + path;
 
     try {
+        const fetchHeaders = { ...headers };
+        // Make sure to NOT forward the wrong host
+        delete fetchHeaders['host'];
+
         const proxyRes = await fetch(finalUrl, {
             method,
-            headers: {
-                ...headers,
-                host: new URL(baseUrl).host,
-            },
-            body: ['GET', 'HEAD'].includes(method) ? undefined : req.body,
+            headers: fetchHeaders,
+            body: ['GET', 'HEAD'].includes(method)
+                ? undefined
+                : req
         });
 
         res.status(proxyRes.status);
-        for (const [key, value] of proxyRes.headers.entries()) {
+        proxyRes.headers.forEach((value, key) => {
             res.setHeader(key, value);
-        }
+        });
 
-        // To bypass CORS, we have to spoof headers
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Headers', '*');
 
